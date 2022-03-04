afc0e008b9307f9e73a34592df047107ec47a530
vcatafesta <vcatafesta@gmail.com>
2022-03-04 19:01:03 -0400

    sex 04 mar 2022 19:01:03 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- core.sh
+++ core.sh
@@ -120,6 +120,19 @@ CURS_ZERO="\\033[0G"
 
 # SUBROUTINES
 
+
+
+#Autor Julio C. Neves
+function string.rfill() {
+     local string=${1// /^}    # Trocando eventuais espaços preexistentes
+     declare -i size=$2
+     local char=$3
+     local fillString
+     printf -v fillString %-${size}s $string
+     fillString=${fillString// /$char}
+     echo "${fillString//^/ }"        # Restaurando espaços anteriores }
+}
+
 function zshdw()
 {
 	sudo git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
@@ -685,6 +698,18 @@ function join()
 	read -t 1 car cdr || return
 	test "${cdr}" || { echo "${car}" ; return ; }
 	echo "${car}${outdelimiter}${cdr}" | ${FUNCNAME} "${indelimiter}" "${outdelimiter}"
+}
+
+function string.alltrim(){
+	local cstr=$1
+	echo "${cstr//[$'\t\r\n ']}"
+}
+
+function string.len()
+{
+	local cstr=$1
+#	echo ${#cstr}
+	printf ${#cstr}
 }
 
 function len()

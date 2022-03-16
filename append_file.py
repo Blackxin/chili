961756d64fc50dbcabb871837427278c21f2a012
vcatafesta <vcatafesta@gmail.com>
2022-03-16 11:20:01 -0400

    qua 16 mar 2022 11:20:01 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- append_file.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-with open("test.txt", "a") as myfile:
-    myfile.write("appended text")
--- testversion.py
+++
@@ -1,8 +0,0 @@
-
-import sys
-
-if sys.version_info < (3, 0):
-    print("Ops, you should be running Python 3")
-    sys.exit(1)
-
-print(sys.version_info)

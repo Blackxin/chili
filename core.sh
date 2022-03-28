d062be5b259a0d40add3f9b460550b34aa8e190c
vcatafesta <vcatafesta@gmail.com>
2022-03-27 22:00:55 -0400

    dom 27 mar 2022 22:00:55 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- core.sh
+++ core.sh
@@ -275,6 +275,24 @@ function colorize(){
     readonly ALL_OFF BOLD BLUE GREEN RED YELLOW
 }
 
+function emailcheck()
+{
+	email_REGEX="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+	email_to_check=${1}
+
+	if [ -z "${email_to_check}" ]; then
+	    echo "É necessário inserir um endereço de e-mail!"
+	    exit 2
+	else
+	    if [[ "${email_to_check}" =~ ${email_REGEX} ]]; then
+	        echo "O endereço '${email_to_check}' é válido!"
+	    else
+	        echo "O endereço '${email_to_check}' não é válido!"
+	        exit 1
+	    fi
+	fi
+}
+
 function plain()
 {
 	local mesg=$1; shift

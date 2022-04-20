f6d864fb4de3bbfe412be8b7315884263d8b146b
vcatafesta <vcatafesta@gmail.com>
2022-04-20 12:55:09 -0400

    qua 20 abr 2022 12:55:09 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- awk.sh
+++ awk.sh
@@ -4,3 +4,9 @@ awk '$1 ~ /^[b,c]/ { print $0}' ~/.bashrc
 awk '{print substr($0, 4)}' /var/cache/fetch/search/packages-split.csv 
 df | awk 'NR==2, NR==5 {print NR, $0}'
 df | awk 'NR==2, NR==5 {print $0}'
+awk '$1 == "case" {print $0}' /tmp/fetch
+awk '{print $0}' /tmp/fetch
+awk 'BEGIN { FS=","; print "Processando registros"} /pacman/ { print $1 }' /var/cache/fetch/search/packages-installed-split.csv 
+awk 'BEGIN { FS=","; print "Processando registros"} /pacman/ { print$1"-"$2}' /var/cache/fetch/search/packages-installed-split.csv 
+awk 'BEGIN { FS=","; print "Processando registros"} /pacman/ { print $4}' /var/cache/fetch/search/packages-installed-split.csv
+awk 'BEGIN { FS=","; print "Processando registros"} { print NR, $4}' /var/cache/fetch/search/packages-installed-split.csv

8831709fd4a5cc7b4881c91933f997c9256bd4fa
vcatafesta <vcatafesta@gmail.com>
2022-04-24 13:33:51 -0400

    dom 24 abr 2022 13:33:51 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- parted-chili.sh
+++ parted-chili.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-parted --script /dev/sdc -- \
-   mklabel gpt             \
+if [[ $# -eq 0 ]]; then
+	printf "Usage: $0 /dev/sdX\n"
+	exit 1;
+fi
+
+sd=$1
+parted --script $sd -- 															\
+   mklabel gpt             													\
    mkpart primary fat32      1MiB 2MiB set 1 bios on name 1 BIOS  \
    mkpart primary fat32      2MiB 3MiB set 2 esp  on name 2 EFI   \
    mkpart primary linux-swap 3MiB 2GB                name 3 SWAP  \
    mkpart primary ext4       2GB 100%                name 4 ROOT  \
    align-check optimal 1
+
+parted --script $sd -- print

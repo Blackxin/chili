72e2225dfa870266fffe100a2d075866280d3630
vcatafesta <vcatafesta@gmail.com>
2022-04-30 00:26:12 -0400

    sáb 30 abr 2022 00:26:12 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- tamanhostring.sh
+++ tamanhostring.sh
@@ -19,3 +19,11 @@ echo 'echo echo ${#Cad}'
 echo ${#Cad}
 #4
 
+
+#medicoes
+
+echo 'time for ((i=1; i<2000; i++)) { echo -n $Cad | wc -m > /dev/null; }'
+time for ((i=1; i<2000; i++))	{ echo -n $Cad | wc -m > /dev/null; }
+echo 'time for ((i=1; i<2000; i++)) { echo ${#Cad} > /dev/null; }'
+time for ((i=1; i<2000; i++))	{ echo ${#Cad} > /dev/null; }
+

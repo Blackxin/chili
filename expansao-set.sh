908006a824171249bcbda0f5e25a604bd50763c3
vcatafesta <vcatafesta@gmail.com>
2022-05-20 20:45:52 -0400

    sex 20 mai 2022 20:45:52 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- expansao-set.sh
+++ expansao-set.sh
@@ -6,6 +6,7 @@ echo ${@:0:1}
 echo ${@:1:1}
 echo ${@: -2:5}
 echo ${@: -5:2}
+echo 'echo ${@: -5:-3} #FAIL, usado somente em string, NOT array ou parametros'
 echo ${@: -5:-3} #fail serve somente em string, not array ou parametros
 
 

0d80f7240afe791fe6403aa446f686acee4c2df8
vcatafesta <vcatafesta@gmail.com>
2022-06-05 16:32:31 -0400

    dom 05 jun 2022 16:32:31 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- core.sh
+++ core.sh
@@ -51,6 +51,21 @@ trancarstderr=2>&-
 : ${ARRAY_DSK_MODEL=()}
 : ${ARRAY_DSK_TRAN=()}
 
+timetoseconds(){
+	[[ $1 && $# -le 3 ]] || { echo "Número incorreto de argumentos!"; exit 1; }
+
+	local argv=($*)
+	local s=${!#}
+	local exp=1
+
+	while [[ $(( ${#argv[*]} -1 )) -ge 1 ]]; do
+		unset argv[-1]
+		s=$(( s + ${argv[${#argv[*]} -1]} * 60**exp ))
+		((exp++))
+	done
+	echo $s
+}
+
 sh_linecount()
 {
    awk 'END {print NR}' $1

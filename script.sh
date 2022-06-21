feba7fec782bdbc3090e9f66c3e6573d23dc7487
vcatafesta <vcatafesta@gmail.com>
2022-06-21 02:02:29 -0400

    ter 21 jun 2022 02:02:28 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+regexsed='^.*>\\K(.+)-(([^-]+)-([0-9]+))-([^.]+)(\.chi\.zst)(<\/a>.*)(\\d{4}-\\d{2}-\\d{2}|\\d{2}-\w{3}-\\d{4})\s(\\d{2}:\\d{2})(.*\s)(\\d+[[:alpha:]]?)'
+   regex='^.*>\K(.+)-(([^-]+)-([0-9]+))-([^.]+)(\.chi\.zst)(<\/a>.*)(\d{4}-\d{2}-\d{2}|\d{2}-\w{3}-\d{4})\s(\d{2}:\d{2})(.*\s)(\d+[[:alpha:]]?)'
 
+curl --compressed --insecure --silent --url "https://chilios.com.br/packages/a/" | \
+grep -Po "$regex" | \
+#sed -nE "/s/$regex/\1 -\2/p"
+awk -e '$0 ~ /"$regex"/ {print $1}'
 

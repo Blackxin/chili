5f82e3efa78ab652d8b6e3d6be9b48536a9bf47c
vcatafesta <vcatafesta@gmail.com>
2022-07-03 05:01:46 -0400

    dom 03 jul 2022 05:01:46 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- chili-parted.sh
+++ chili-parted.sh
@@ -41,13 +41,21 @@ if [[ $# -eq 0 ]]; then
 	exit 1;
 fi
 
-sd=$1
-parted --script $sd -- 															\
-   mklabel gpt             													\
-   mkpart primary fat32      1MiB   100MiB set 1 bios on name 1 BIOS  \
-   mkpart primary fat32      100MiB 200MiB set 2 esp  on name 2 EFI   \
-   mkpart primary linux-swap 200MiB 2GB                  name 3 SWAP  \
-   mkpart primary ext4       2GB 100%                    name 4 ROOT  \
-   align-check optimal 1
+#sd=$1
+#parted --script $sd -- 															\
+#   mklabel gpt             													\
+#   mkpart primary fat32      1MiB   100MiB set 1 bios on name 1 BIOS  \
+#   mkpart primary fat32      100MiB 200MiB set 2 esp  on name 2 EFI   \
+#   mkpart primary linux-swap 200MiB 2GB                  name 3 SWAP  \
+#   mkpart primary ext4       2GB 100%                    name 4 ROOT  \
+#   align-check optimal 1
+#parted --script $sd -- print
 
+sd=$1
+parted --script $sd -- 																\
+   mklabel gpt             					 									\
+   mkpart primary fat32      1MiB 2MiB set 1 bios on name 1 BIOS  	\
+   mkpart primary fat32      2MiB 50MiB set 2 esp  on name 2 EFI   	\
+   mkpart primary ext4       50MiB 100%                name 3 ROOT  	\
+   align-check optimal 1
 parted --script $sd -- print
